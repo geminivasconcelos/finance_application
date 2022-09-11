1d1e890f2fdb6889e811c7eb01b8547d49c3f485
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-09-11 12:37:11 -0300

    refactor: headerLc ajuste desktop

--- finance-app-front/src/components/HeaderLC/index.jsx
+++ finance-app-front/src/components/HeaderLC/index.jsx
@@ -5,7 +5,7 @@ import Header from '../../assets/header_lc.svg'
 export default function HeaderLC() {
     return (
         <div className='div_header'>
-            <img src={Header} alt="Imagem do header" />
+            <img src={Header} alt="Imagem do header" className='headerLC_img'/>
         </div>
     )
 }
