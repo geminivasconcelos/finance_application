32e53883c1364d5ba37cb658a767e47d5cee6d68
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-09-10 21:16:56 -0300

    feat: adicao da rota

---
+++ finance-app-front/src/routes.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+
+import { Login } from './pages/Login';
+import { Cadastro } from './pages/Cadastro';
+
+function ProtectedRoute(props) {
+    const token = localStorage.getItem('token');
+    return token ? <Outlet /> : <Navigate to={props.redirect} />;
+}
+
+export default function MyRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/Cadastro' element={<Cadastro />} />
+                <Route path='/Login' element={<Login />} />
+
+                <Route element={<ProtectedRoute redirect="/Login" />}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
