a060c2bfa0c484433ea110f18a4c2d36e0f39956
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-09-11 10:33:42 -0300

    feat: criacao de rotas

--- finance-app-front/src/routes.jsx
+++ finance-app-front/src/routes.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { Login } from './pages/Login';
-import { Cadastro } from './pages/Cadastro';
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
 
-function ProtectedRoute(props) {
-    const token = localStorage.getItem('token');
-    return token ? <Outlet /> : <Navigate to={props.redirect} />;
-}
+// function ProtectedRoute(props) {
+//     const token = localStorage.getItem('token');
+//     return token ? <Outlet /> : <Navigate to={props.redirect} />;
+// }
 
 export default function MyRoutes() {
     return (
@@ -16,7 +16,7 @@ export default function MyRoutes() {
                 <Route path='/Cadastro' element={<Cadastro />} />
                 <Route path='/Login' element={<Login />} />
 
-                <Route element={<ProtectedRoute redirect="/Login" />}></Route>
+                {/* <Route element={<ProtectedRoute redirect="/Login" />}></Route> */}
             </Routes>
         </BrowserRouter>
     )
