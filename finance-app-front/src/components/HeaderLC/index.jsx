e1089214f6d018152ec0a5ec9373df23618db73f
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-09-11 11:24:08 -0300

    refactor: refatorando pra desktop

---
+++ finance-app-front/src/components/HeaderLC/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+import Header from '../../assets/header_lc.svg'
+
+
+export default function HeaderLC() {
+    return (
+        <div className='div_header'>
+            <img src={Header} alt="Imagem do header" />
+        </div>
+    )
+}
